b97153d90e540074e15202953ac8679be15c1afc
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-13 10:50:25 +0200

    Edit: tetsing in mainservice

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -14,7 +14,19 @@ public class MainService {
 		heapForIntegers.enqueue(41);
 		
 		try {
+			System.out.println("----------------ENQUEUE------------------");
 			heapForIntegers.print();
+			System.out.println("----------------DEQUEU------------------");
+			System.out.println(heapForIntegers.dequeue());//41
+			System.out.println(heapForIntegers.dequeue());//33
+			System.out.println("----------------AFTER DEQUEUE------------------");
+			heapForIntegers.print();//tikai 32 (vecāks) -> 5 (kreisais bērns) -> 2 (labais bērns)
+			
+			System.out.println("----------------MAKE EMPTY------------------");
+			heapForIntegers.makeEmpty();
+			heapForIntegers.enqueue(100);
+			heapForIntegers.enqueue(1000);
+			heapForIntegers.print();//1000 (vecāks) un 100 (kreisais bērns)
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
